fbb0764ad012318d7ab6f09d4243d33538637465
Baiel <baielkarybaev@gmail.com>
2022-10-01 06:57:38 +0600

    create background color

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-const Contact =()=>{
-    return(
-        <div>Contact page</div>
+
+const Contact = () => {
+    const ANIMELS = 'https://pokeapi.co/api/v2/pokemon?limit=400'
+    const [animes, setAnime] = useState([])
+    useEffect(() => {
+        fetch(ANIMELS)
+            .then(res => res.json())
+            .then(data => setAnime(data.results))
+    }, [])
+    console.log(animes);
+    return (
+        <div>
+            {animes.splice(0,4).map(anime => {
+                return (
+                <div>
+                    <img src="i'm"/>
+                    <p>{anime.name}</p>
+                </div>
+                )
+            })}
+
+        </div>
     )
 }
 export default Contact
