03ad7c50477b19a20f9b254a2066769633125101
Baiel <baielkarybaev@gmail.com>
2022-10-02 22:58:07 +0600

    create contact

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 const Contact = () => {
-    
+
     return (
         <>
-        <div className="contactContentOne">
-            
-        </div>
+            <div className="contactContentOne">
+                <a className="instagramText" href="https://www.instagram.com/karybaevv_.09/"><img className="instagramIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a5/Instagram_icon.png/2048px-Instagram_icon.png"/>instagram</a>
+                <a className="instagramText" href="+996700047017"><img className="instagramIcon" src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/598px-WhatsApp_icon.png"/>WhatsApp</a>
+            </div>
         </>
     )
 }
